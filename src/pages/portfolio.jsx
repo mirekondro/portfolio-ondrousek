ad55b889be89b615e5ada159b54ac07372884727
2021OndrousekMirosla <mirek.ondrousek999@gmail.com>
2023-11-08 12:08:49 +0100

    update project

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <section id="section1">
+                <h2>Sekce 1</h2>
+                {"fuehfuejgiejfiefiejfeifjeigj"}
+            </section>
+            <section id="section2">
+                <h2>Sekce 2</h2>
+                {"fuehfuejgiejfiefiejfeifjeigj"}
+            </section>
+            <section id="section3">
+                <h2>Sekce 3</h2>
+                {"fuehfuejgiejfiefiejfeifjeigj"}
+            </section>
+        </div>
+    );
+}
+export default Portfolio;
