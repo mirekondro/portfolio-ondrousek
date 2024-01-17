76ce2dddc5482999f7abc187b96e331a237617ef
2021OndrousekMirosla <mirek.ondrousek999@gmail.com>
2024-01-17 11:33:00 +0100

    update

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './_homepage.css';
 import {introdata, socials} from '../../my_data';
+import {motion} from "framer-motion";
 
 function Homepage() {
     return (
         <div>
             <section className="hero">
                 <div className="homepage">
+                    <motion.div
+                        initial= {{scale: 1, opacity: 0, x: -270}}
+                        whileInView={{opacity: 1, x: 0}}
+                        transition={{duration: 0.5}}
+                        viewport={{once: false}}
+                    >
                     <div className="jmeno">
                         <h1>Hi! I´m Mirek</h1><br></br>
                         <h1>UX/UI Designer</h1>
@@ -15,6 +22,7 @@ function Homepage() {
                     <div className="obrazek_home">
                         <img src="homepageIMG.png" alt="špíňák"></img>
                     </div>
+                </motion.div>
                 </div>
 
             </section>
