852cf2bca676652ca62a2d78cb3953d6b710b7bd
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-01-20 21:21:30 +0100

    update

--- src/pages/aboutme/aboutme.jsx
+++ src/pages/aboutme/aboutme.jsx
@@ -23,25 +23,11 @@ function AboutMe() {
 
 
                 <div className="text-block-about">
-                    <motion.div
-                        initial= {{scale: 1, opacity: 0, x: 0}}
-                        whileInView={{opacity: 1, x: 0}}
-                        transition={{duration: 2.5}}
-                        viewport={{once: false}}
-                    >
                     <h2>Lorem ipsum dolor sit amet, consectetue. Fusce aliquam vestibulum ipsum.</h2>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                    </motion.div>
 
-                    <motion.div
-                        initial= {{scale: 1, opacity: 0, x: 0}}
-                        whileInView={{opacity: 1, x: 0}}
-                        transition={{duration: 2.5}}
-                        viewport={{once: false}}
-                    >
                     <a href="#" className="getintouch-btn">Get In Touch</a>
                     <a href="#" className="portfolio-btn">Portfolio</a>
-                    </motion.div>
                 </div>
             </section>
         </div>
